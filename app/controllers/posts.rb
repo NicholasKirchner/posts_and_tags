9ad6890bb666af368dc05d055acc47c96d1d16f2
Nicholas Kirchner <kirc0076@umn.edu>
2013-06-06 13:27:27 -0700

    Created a page for editing posts

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -2,13 +2,13 @@ get '/posts' do
   erb :list_posts
 end
 
-get '/posts/:id' do
+get '/post/:id' do
   @post = Post.find(params[:id].to_i)
   erb :view_post
 end
 
 get '/posts/create' do
-
+  erb :create_post
 end
 
 post '/posts/create' do
@@ -16,7 +16,8 @@ post '/posts/create' do
 end
 
 get '/posts/edit/:id' do
-
+  @post = Post.find(params[:id].to_i)
+  erb :create_post
 end
 
 post '/posts/delete/:id' do
